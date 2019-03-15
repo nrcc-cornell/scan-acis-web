cd94f7fb5532c1a96e3272b1d44765c8e3bd7b43
Brian Belcher <belcher.bn@gmail.com>
2019-03-15 10:22:47 -0400

    update router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -65,7 +65,7 @@ class App extends Component {
     render() {
 
         return (
-          <Router>
+          <Router basename={process.env.PUBLIC_URL}>
             <MuiThemeProvider theme={theme}>
               <div className="App">
                 <Header />
