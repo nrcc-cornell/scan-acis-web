407737afba6a1a653e16eba5ff8b6ab3d0145b78
Brian Belcher <belcher.bn@gmail.com>
2019-09-04 15:28:55 -0400

    updates

--- src/components/stem/WaterdefDoc/index.js
+++ src/components/stem/WaterdefDoc/index.js
@@ -73,15 +73,27 @@ class WaterdefDoc extends Component {
                       What does this tool do?
                     </Typography>
                     <Typography align="left" paragraph variant="body1">
-                      Lorem ipsum dolor sit amet, eu tristique, etiam vel donec, lorem ut ridiculus aliquam, eget faucibus at nunc, cursus justo nonummy et. Imperdiet ut pellentesque eu lacus tincidunt, dolor pede velit felis tempus et justo. Id sit eget sollicitudin, vitae et, tellus cum a massa quam scelerisque sed. Sed duis suscipit, sed quisque potenti quis donec mollitia. Justo urna fusce sed id nisl eu, lobortis tincidunt lobortis, aenean diam orci, mauris elit in, quam bibendum sed commodo est sed. Pellentesque a nam, non in nullam rhoncus neque aliquam ac, mollis convallis. Sed vestibulum, dolor vulputate leo a, a in posuere et a. Nam commodo condimentum, est ipsum mattis sollicitudin ante commodo tristique. Mollitia taciti luctus lacus quisque. Nec maecenas ut dui maecenas sapien. Tincidunt nullam occaecat in, mauris exercitation ipsum vitae, nunc integer non, at quis turpis varius, nibh adipiscing velit mi.
+                      This tool calculates the soil water deficit for a selected range of soil depths. Deficits are presented relative to the field capacity of soil at the site of interest. Therefore, the calculated water deficit reprepresents the amount of water, from either precipitation or irrigation methods, that would be necessary to bring soil moisture amounts back to field capacity.
                     </Typography>
               </Grid>
               <Grid item>
                     <Typography align="left" paragraph variant="h6">
-                      How to use this tool.
+                      Sources of data
                     </Typography>
                     <Typography align="left" paragraph variant="body1">
-Vestibulum eu sociis litora sed mauris, fusce consequat gravida mi dictum cras turpis, lectus suscipit eros phasellus nunc vel. Euismod eros duis, tortor aliquam, ipsum scelerisque viverra, nonummy at nisl neque neque ornare, ultricies quam venenatis est nunc duis. A wisi rhoncus. Sit quasi augue aliquam in, nibh vel posuere, sollicitudin nec mi mollis, imperdiet quam maecenas ac tellus, aptent mi pede auctor non. Luctus placerat metus enim risus dui, lectus fermentum libero, erat risus sit ut mauris arcu, lorem vestibulum purus mattis vestibulum in non, ullamcorper a donec praesent fringilla ut. Amet quisque vehicula, venenatis rhoncus metus blandit pede et orci, tempus euismod arcu, diam rhoncus ut, magna tempor eget tristique vel quasi mollis. In in nibh pharetra mi. Pellentesque vel eget nobis, hymenaeos at hendrerit, augue sapien, maecenas pharetra, interdum id gravida cum mauris hymenaeos. Praesent in ut qui, lacus gravida, quasi urna sagittis a, accumsan dui metus sit aliquam lobortis lectus. Minima a amet, neque odio, sit enim netus adipiscing porttitor aenean et.
+                      Soil characterization data from the National Soil Survey Center is available for most SCAN sites. For this tool, the van Genuchten retention parameters are used from these data to estimate the field capacity and wilting point for each soil horizon at the site of interest. Observations from SCAN soil sensors are then obtained to provide this tool with daily snapshots of volumetric soil moisture at up to five soil depths (2, 4, 8, 20 and 40 inches). Together, these data provide the means to estimate the available water deficit at any depth, or range of depths, within the soil profile.
+                    </Typography>
+              </Grid>
+              <Grid item>
+                    <Typography align="left" paragraph variant="h6">
+                      How to use this tool
+                    </Typography>
+                    <Typography align="left" paragraph variant="body1">
+                      <i>1. User Options</i><br/>
+                      Once your site is selected, there are two options to choose from at the top of the tool: the year, and the range of soil depths. The year selection defaults to the current year, while the soil depth defaults to the top 12 inches of the soil profile. To change the soil depth, click and drag the handles of the selected range to the depths of interest.
+                      <i>2. Output</i><br/>
+                      Water deficit values that correspond to your selections will appear below the user options. 
+                      
                     </Typography>
               </Grid>
             </Grid>
